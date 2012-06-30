70b605c4916a47db7de488c9af434eed77aa6eef
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-06-30 04:15:31 +0200

    Use gunicorn

--- blog/settings.py
+++ blog/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'gunicorn',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
