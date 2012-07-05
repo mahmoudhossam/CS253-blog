c37c8ccd25dab57664084c70689bfe8accd9f4bc
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-07-05 15:17:42 +0200

    Add users

--- webapp/models.py
+++ webapp/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    hashed_pw = models.CharField(max_length=100)
+    salt = models.CharField(max_length=50)
+    email = models.EmailField(blank=True)
+
 class Post(models.Model):
-    subject = models.CharField(max_length=30, blank=False)
-    content = models.TextField(blank=False)
+    subject = models.CharField(max_length=30)
+    content = models.TextField()
 
 def get_all_posts():
     return Post.objects.all()
+
+def get_user(user_id):
+    return User.objects.get(id=user_id)
